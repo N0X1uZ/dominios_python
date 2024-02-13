e1ae3945118334373916c2cf33a4449fd1690814
Aprendiz <Aprendiz@BOGDFPCGMFN4138.FormacionSena.RED>
2024-02-13 17:39:27 -0500

    evidencia

--- app/entities.py
+++ app/entities.py
@@ -43,3 +43,8 @@ class pago():
         self.carro = carro
         self.cupo = cupo
         self.empleado = empleado
+        
+class empleado():
+    def __init__(self, nombre, codigo):
+        self.nombre = nombre
+        self.codigo = codigo
